0609570804d9742005e0d842c6b54ff500308ce5
kalin73 <104739268+kalin73@users.noreply.github.com>
2022-11-04 20:57:10 +0200

    solution

---
+++ Polymorphism/src/Math_Operation/MathOperation.java
@@ -0,0 +1,22 @@
+package Math_Operation;
+
+public class MathOperation {
+
+	public int add(int n1, int n2) {
+
+		return n1 + n2;
+
+	}
+
+	public int add(int n1, int n2, int n3) {
+
+		return n1 + n2 + n3;
+
+	}
+
+	public int add(int n1, int n2, int n3, int n4) {
+
+		return n1 + n2 + n3 + n4;
+
+	}
+}
