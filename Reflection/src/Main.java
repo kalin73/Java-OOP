5f358610470fb79078b097917abf80ff29de62e1
kalin73 <104739268+kalin73@users.noreply.github.com>
2022-11-20 17:52:12 +0200

    solution

---
+++ Reflection/src/Main.java
@@ -0,0 +1,23 @@
+import java.lang.reflect.InvocationTargetException;
+
+public class Main {
+
+	public static void main(String[] args) throws InstantiationException, IllegalAccessException,
+			IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
+		Class reflection = Reflection.class;
+		System.out.println(reflection);
+
+		Class superclass = reflection.getSuperclass();
+		System.out.println(superclass);
+
+		Class[] interfaces = reflection.getInterfaces();
+
+		for (Class anInterface : interfaces) {
+			System.out.println(anInterface);
+		}
+
+		Object reflectionObject = reflection.getDeclaredConstructor().newInstance();
+		System.out.println(reflectionObject);
+	}
+
+}
