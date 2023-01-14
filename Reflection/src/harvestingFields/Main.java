1b68ce8b7a3fe600c77831277f3bdc98597bb91f
kalin73 <104739268+kalin73@users.noreply.github.com>
2023-01-14 19:46:15 +0200

    fix

--- Reflection/src/harvestingFields/Main.java
+++ Reflection/src/harvestingFields/Main.java
@@ -2,71 +2,50 @@ package harvestingFields;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.function.Consumer;
 
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		RichSoilLand reflection = new RichSoilLand();
 
-		Field[] fields = reflection.getClass().getDeclaredFields();
+		List<Field> fields = Arrays.asList(RichSoilLand.class.getDeclaredFields());
 		String command = sc.nextLine();
+
+		Consumer<Field> print = field -> System.out.println(Modifier.toString(field.getModifiers()) + " "
+				+ field.getType().getSimpleName() + " " + field.getName());
 
 		while (!"HARVEST".equals(command)) {
 
 			switch (command) {
 			case "private":
-				printPrivate(fields);
+				fields.stream()
+					  .filter(x -> Modifier.isPrivate(x.getModifiers()))
+					  .forEach(print);
 				break;
 
 			case "protected":
-				printProtected(fields);
+				fields.stream()
+				      .filter(x -> Modifier.isProtected(x.getModifiers()))
+				      .forEach(print);
 				break;
 
 			case "public":
-				printPublic(fields);
+				fields.stream()
+					  .filter(x -> Modifier.isPublic(x.getModifiers()))
+					  .forEach(print);
 				break;
 
 			case "all":
-				printAll(fields);
+				fields.stream()
+					  .forEach(x -> System.out.println(
+						Modifier.toString(x.getModifiers()) + " " + x.getType().getSimpleName() + " " + x.getName()));
 				break;
 			}
 			command = sc.nextLine();
 		}
 		sc.close();
-	}
-
-	private static void printPrivate(Field[] fields) {
-		for (Field f : fields) {
-			int modifier = f.getModifiers();
-			if (Modifier.isPrivate(modifier)) {
-				System.out.println(Modifier.toString(modifier) + " " + f.getType().getSimpleName() + " " + f.getName());
-			}
-		}
-	}
-
-	private static void printProtected(Field[] fields) {
-		for (Field f : fields) {
-			int modifier = f.getModifiers();
-			if (Modifier.isProtected(modifier)) {
-				System.out.println(Modifier.toString(modifier) + " " + f.getType().getSimpleName() + " " + f.getName());
-			}
-		}
-	}
-
-	private static void printPublic(Field[] fields) {
-		for (Field f : fields) {
-			int modifier = f.getModifiers();
-			if (Modifier.isPublic(modifier)) {
-				System.out.println(Modifier.toString(modifier) + " " + f.getType().getSimpleName() + " " + f.getName());
-			}
-		}
-	}
-
-	private static void printAll(Field[] fields) {
-		for (Field f : fields) {
-			int modifier = f.getModifiers();
-			System.out.println(Modifier.toString(modifier) + " " + f.getType().getSimpleName() + " " + f.getName());
-		}
 	}
 }
