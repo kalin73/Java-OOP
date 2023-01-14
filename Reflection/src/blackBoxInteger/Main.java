9d71d5d88ec1307efd26e8954c65f0f8e822cbee
kalin73 <104739268+kalin73@users.noreply.github.com>
2023-01-14 20:36:08 +0200

    fix

--- Reflection/src/blackBoxInteger/Main.java
+++ Reflection/src/blackBoxInteger/Main.java
@@ -13,6 +13,9 @@ public class Main {
 		constructor.setAccessible(true);
 		BlackBoxInt b = constructor.newInstance();
 
+		Field field = b.getClass().getDeclaredField("innerValue");
+		field.setAccessible(true);
+
 		String[] input = sc.nextLine().split("_");
 
 		while (!"END".equals(input[0])) {
@@ -23,22 +26,10 @@ public class Main {
 
 			method.setAccessible(true);
 			method.invoke(b, value);
-			getResult(b);
+			System.out.println(field.get(b));
+
 			input = sc.nextLine().split("_");
 		}
 		sc.close();
-
-	}
-
-	private static void getResult(BlackBoxInt reflection) throws Exception {
-		Field[] fields = reflection.getClass().getDeclaredFields();
-
-		for (Field f : fields) {
-			if (f.getName().equals("innerValue")) {
-				f.setAccessible(true);
-
-				System.out.println(f.getInt(reflection));
-			}
-		}
 	}
 }
